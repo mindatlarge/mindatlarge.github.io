b46b21ecef87db51142a1ff2038407422668cf7b
aeroplancow <Nemanja@MacBook-Pro.local>
2018-04-17 01:32:15 +0200

    commit

--- fade-out.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function () {
-    $('.dropdown-container-saat div, .dropdown-container-cg div').each(function () {
-        if (($(this).offset().top - $(window).scrollTop()) < 20) {
-            $(this).stop().fadeTo(100, 0);
-        } else {
-            $(this).stop().fadeTo('fast', 1);
-        }
-    });
-});
