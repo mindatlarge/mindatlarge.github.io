51e549def97096292872ba3be0170e160784b707
aeroplancow <Nemanja@MacBook-Pro.local>
2018-04-25 02:13:11 +0200

    commit

--- Magnific-Popup-master/dist/jquery.magnific-popup.js
+++ Magnific-Popup-master/dist/jquery.magnific-popup.js
@@ -89,7 +89,7 @@ var _mfpOn = function(name, f) {
 	},
 	_getCloseBtn = function(type) {
 		if(type !== _currPopupType || !mfp.currTemplate.closeBtn) {
-			mfp.currTemplate.closeBtn = $( mfp.st.closeMarkup.replace('%title%', mfp.st.tClose ) );
+			//mfp.currTemplate.closeBtn = $( mfp.st.closeMarkup.replace('%title%', mfp.st.tClose ) );
 			_currPopupType = type;
 		}
 		return mfp.currTemplate.closeBtn;
