d3948358e4dc9c157fc570de06d4d29dec83b571
mindatlarge <karebre@gmail.com>
2018-05-03 20:15:32 +0200

    danas

--- otvaranje-galerija.js
+++ otvaranje-galerija.js
@@ -1,22 +1,49 @@
 $(document).ready(function() {
 	
-	/* DOK IMA JEDNA GALERIJA U SOLO */
+	if ($(window).width() > 575 ) {
+		// CARLIER_GEBAUER DESKTOP
 	$('.otvori-cg').on('click', function(){ 
 		$('.dropdown-container-cg').fadeToggle(100);
     	$('#container').addClass('pozadina-galerija');
     	$('.dropdown-container-cg').slick('setPosition');
     	$('.close-btn').fadeToggle();
 	});
-
-	/* DOK IMA JEDNA GALERIJA U GROUP */
+		// SAATCHI DESKTOP
 	$('.otvori-saat').on('click', function(){    
     	$('.dropdown-container-saat').fadeToggle(100);
     	$('#container').addClass('pozadina-galerija');
     	$('.dropdown-container-saat').slick('setPosition');
     	$('.close-btn').toggle();
 	});
+
+		// ZATVORI GALERIJU DESKTOP
 	$('.close-btn').on('click', function(){
 		$('.dropdown-container-cg, .dropdown-container-saat, .close-btn').fadeOut(200);
 		$('#container').removeClass('pozadina-galerija');
 	});
+
+
+		} else {
+
+			// CARLIER_GEBAUER MOB
+	$('.otvori-cg').on('click', function(){
+		$('.dropdown-container-cg').fadeToggle(100);
+		$('#container').hide();
+		$('.close-btn-m').show();
+	});
+			// SAATCHI MOB
+	$('.otvori-saat').on('click', function(){
+		$('dropdown-container-saat').fadeToggle(100);
+		$('#container').hide();
+		$('.close-btn-m').show();
+	});
+			// ZATVORI GALERIJU MOB
+	$('.close-btn-m').on('click', function(){
+		$('.dropdown-container-cg, .dropdown-container-saat, .close-btn-m').fadeOut(200);
+		$('#container').fadeIn(200);
+	});
+
+	}
 });
+
+
