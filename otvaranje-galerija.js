148c17583c4ada72066c8438fb16e76f0e48bcd9
mindatlarge <karebre@gmail.com>
2018-05-12 19:07:36 +0200

    magnific popup exhibition

--- otvaranje-galerija.js
+++ otvaranje-galerija.js
@@ -2,29 +2,60 @@
 $(document).ready(function() {
 	//mobile
 	if ($(window).width() > 767 && ($(window).height() > 414)) {
-		// CARLIER_GEBAUER DESKTOP
-	$('.otvori-cg').on('click', function(){ 
-		$('.dropdown-container-cg').fadeToggle(100);
-    	$('#container').addClass('pozadina-galerija');
-    	$('.dropdown-container-cg').slick('setPosition');
-    	$('.close-btn').fadeToggle();
-	});
-		// SAATCHI DESKTOP
-	$('.otvori-saat').on('click', function(){    
-    	$('.dropdown-container-saat').fadeToggle(100);
-    	$('#container').addClass('pozadina-galerija');
-    	$('.dropdown-container-saat').slick('setPosition');
-    	$('.close-btn').toggle();
-	});
+		//CARLIER-GEBAUER
+		$('.otvori-cg').magnificPopup({
+			items: [{
+				src: 'exhibitions/CGEBAUER-W/CGEBAUER-1.jpg',
+				title: 'Exhibition view at carlier | gebauer, 2017. Courtesy of the artist and carlier | gebauer. Photo: Trevor Good.'},
+			{	src: 'exhibitions/CGEBAUER-W/CGEBAUER-2.jpg',
+				title: 'Exhibition view at carlier | gebauer, 2017. Courtesy of the artist and carlier | gebauer. Photo: Trevor Good.'},
+			{	src: 'exhibitions/CGEBAUER-W/CGEBAUER-3.jpg',
+				title: 'Exhibition view at carlier | gebauer, 2017. Courtesy of the artist and carlier | gebauer. Photo: Trevor Good.'},
+			{	src: 'exhibitions/CGEBAUER-W/CGEBAUER-4.jpg',
+				title: 'Exhibition view at carlier | gebauer, 2017. Courtesy of the artist and carlier | gebauer. Photo: Trevor Good.'},
+			{	src: 'exhibitions/CGEBAUER-W/CGEBAUER-5.jpg',
+				title: 'Exhibition view at carlier | gebauer, 2017. Courtesy of the artist and carlier | gebauer. Photo: Trevor Good.'},
+		],
+		gallery: {enabled: true},
+		callbacks: {
+	        change: function() {
+        var mfp = $.magnificPopup.instance;
+        var container = $(mfp.container);
 
-		// ZATVORI GALERIJU DESKTOP
-	$('.close-btn').on('click', function(){
-		$('.dropdown-container-cg, .dropdown-container-saat, .close-btn').fadeOut(200);
-		$('#container').removeClass('pozadina-galerija');
-	});
+        if (mfp.index >= mfp.items.length - 1) container.addClass('mfp-last');
+        else container.removeClass('mfp-last');
+        if (mfp.index == 0) container.addClass('mfp-first');
+        else container.removeClass('mfp-first');
+    },
+    open: function() {
+        var mfp = $.magnificPopup.instance;
+        var proto = $.magnificPopup.proto;
+
+        // extend function that moves to next item
+        mfp.next = function() {
+
+            // if index is not last, call parent method
+            if(mfp.index < mfp.items.length - 1) {
+                proto.next.call(mfp);
+            } else {
+               // otherwise do whatever you want, e.g. hide "next" arrow
+            }
+        };
+
+        // same with prev method
+        mfp.prev = function() {
+            if(mfp.index > 0) {
+                proto.prev.call(mfp);
+            }
+        };
+
+    }
+},
+		type: 'image'
+		});
 
 		} else {
-
+			// MOBILNA
 	$('.otvori-cg').on('click', function(){
 		$('.dropdown-container-cg').fadeToggle(100);
 		$('#container').hide();
@@ -41,8 +72,9 @@ $(document).ready(function() {
 		$('#container').fadeIn(200);
 
 	});
-
 	}
+
+
 });
 
 
