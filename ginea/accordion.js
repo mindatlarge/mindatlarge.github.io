470a12c712d93cf10f65f8e8f264cfe86d2616b5
aeroplancow <brno7107@gmail.com>
2022-05-13 23:56:26 +0200

    update

--- ginea/accordion.js
+++ ginea/accordion.js
@@ -3,6 +3,14 @@ $(function() {
     
     var dropDown = $(this).closest('.acc__card').find('.acc__panel');
     $(this).closest('.acc').find('.acc__panel').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active');
+        } else {
+          $(this).closest('.acc').find('.acc__title.active').removeClass('active');
+          $(this).addClass('active');
+        }
+    
     
     dropDown.stop(false, true).slideToggle();
     j.preventDefault();
