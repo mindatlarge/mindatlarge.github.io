6ae65e40e43855a47456043895b82e1c9d962a62
aeroplancow <brno7107@gmail.com>
2023-06-26 12:22:08 +0200

    slider counter

--- dist/Slick-intitialise.js
+++ dist/Slick-intitialise.js
@@ -1,9 +1,27 @@
-$('.gallery').slick({
+// $('.gallery').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   fade: true,
+//   cssEase: 'linear',
+//   adaptiveHeight: false
+// });
+
+
+var $status = $('.pagingInfo');
+var $slickElement = $('.gallery');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '-' + slick.slideCount);
+});
+
+$slickElement.slick({
   dots: false,
   infinite: false,
   speed: 300,
   fade: true,
-  cssEase: 'linear',
-  adaptiveHeight: false,
+  cssEase: 'linear'
 });
 
